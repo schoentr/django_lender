944f1d4a13d07efc1c35d2eee65296253dc54b76
Tim Schoen <schoentr@gmail.com>
2019-03-31 16:35:34 -0700

    styling started

--- books_app/views.py
+++ books_app/views.py
@@ -1,20 +1,27 @@
-from django.shortcuts import render, get_list_or_404,get_object_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Book
 
 # Create your views here.
 @login_required
 def book_list_view(request):
-
-    books = get_list_or_404(Book,user=request.user.id)
-    context= {
-        'books':books
+    """
+    This fuction gets the books from the db and renders the book list template
+    """
+    books = get_list_or_404(Book, user=request.user.id)
+    context = {
+        'books': books
     }
-    return render(request,'books/book_list.html', context)
+    return render(request, 'books/book_list.html', context)
+
+
 @login_required
 def book_detail_view(request, pk=None):
-    book = get_object_or_404(Book,id=pk,user=request.user.id)
+    """ 
+    This fuction gets the detail of a single book and renders the detail template
+    """
+    book = get_object_or_404(Book, id=pk, user=request.user.id)
     context = {
         'book': book
     }
-    return render(request,'books/book_detail.html',context)
+    return render(request, 'books/book_detail.html', context)
--- django_lender_project/views.py
+++ django_lender_project/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def home_view(request):
+    """
+    This fuction renders the  home page
+    """
+
     return render(request, 'generic/home.html',)  
