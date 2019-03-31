a7c300ad5250b0262fc123342c1d4822e7346953
Tim Schoen <schoentr@gmail.com>
2019-03-31 15:13:38 -0700

    auth working

--- books_app/models.py
+++ books_app/models.py
@@ -5,11 +5,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Book(models.Model):
+    user= models.ForeignKey(User,on_delete=models.CASCADE,related_name='books')
     title = models.CharField(max_length=48)
     author = models.CharField(max_length=4096)
     year = models.IntegerField()
     date_added = models.DateField(default=timezone.now)
-    last_borrowed = models.DateTimeField(auto_now=True)
+    last_borrowed = models.DateTimeField(blank=True,auto_now=True)
 
     STATES = [
         ('available', 'Available'),
--- books_app/tests.py
+++ books_app/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase, RequestFactory
 from .models import Book
+from django.contrib.auth.models import User
 
 
 class TestBookaModels(TestCase):
     def setUp(self):
-        Book.objects.create(title='title one', author='author one', year=1901)
-        Book.objects.create(title='title two', author='author two', year=1902)
-        Book.objects.create(title='title three', author='author three', year=1903)
+        user=User.objects.create_user('sch','one')
+        Book.objects.create(title='title one', author='author one', year=1901,user=user)
+        Book.objects.create(title='title two', author='author two', year=1902, user=user)
+        Book.objects.create(title='title three', author='author three', year=1903,user=user)
 
     def test_book_titles(self):
         one = Book.objects.get(title='title one')
@@ -28,36 +30,41 @@ class TestBookaModels(TestCase):
         
 
 
-class TestNotesViews(TestCase):
+class TestBooksViews(TestCase):
     def setUp(self):
         self.request = RequestFactory()
-        Book.objects.create(title='title one', author='author one', year='1901')
-        Book.objects.create(title='title two', author='author two', year='1902')
-        Book.objects.create(title='title three', author='author three', year='1903')
+        self.user=User.objects.create_user('sch','one')
+        Book.objects.create(title='title one', author='author one', year='1901', user=self.user)
+        Book.objects.create(title='title two', author='author two', year='1902', user=self.user)
+        Book.objects.create(title='title three', author='author three', year='1903', user=self.user)
 
 
 
     def test_book_detail_view_context(self):
         from .views import book_detail_view
         request = self.request.get('')
+        request.user= self.user
         response = book_detail_view(request, f'{Book.objects.get(title="title one").id }')
         self.assertIn(b'author one', response.content)
 
     def test_book_list_view_context(self):
         from .views import book_list_view
         request = self.request.get('')
+        request.user= self.user
         response = book_list_view(request)
         self.assertIn(b'title two', response.content)
 
     def test_book_detail_view_status_code_success(self):
         from .views import book_detail_view
         request = self.request.get('')
+        request.user = self.user
         response = book_detail_view(request, f'{ Book.objects.get(title="title one").id }')
         self.assertEqual(response.status_code, 200)
 
     def test_book_list_view_status_code_success(self):
         from .views import book_list_view
         request = self.request.get('')
+        request.user= self.user
         response = book_list_view(request)
         self.assertEqual(response.status_code, 200)
 
@@ -65,5 +72,6 @@ class TestNotesViews(TestCase):
         from .views import book_detail_view
         from django.http import Http404
         request = self.request.get('')
+        request.user= self.user
         with self.assertRaises(Http404):
             book_detail_view(request, '0')
